6aecfa59a158223a6439d24c8f103385bec63c00
Swimi-21 <157112272+Swimi-21@users.noreply.github.com>
2024-01-25 14:10:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
     else:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-
+      
+#random text 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
