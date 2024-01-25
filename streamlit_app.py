84243c42a218b494747500cf1c27886e3fdc005f
Swimi-21 <157112272+Swimi-21@users.noreply.github.com>
2024-01-25 14:44:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ except UTLError as e:
 #random text 
 streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
-   with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
-        return my_cur.fetchall()
+with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
 
 # Add a button to load the fruit
 if streamlit.button('Get Fruit List'):
